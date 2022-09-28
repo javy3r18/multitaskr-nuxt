2716c35c2df62f9424d83eea63ebcec6a84a9aea
javy3r18 <javy18ms@gmail.com>
2022-09-28 11:45:21 -0700

    filter user data update

--- pages/users/profile.vue
+++ pages/users/profile.vue
@@ -1,30 +1,10 @@
 <template>
-    <b-container>
-        <h1>User data</h1>
-        <ol v-for="(value, key) in user" class="list-group list-group-numbered">
-            <li class="list-group-item"><strong>{{key | upperCase}}:</strong> {{value | isEmpty | isArray}} <b-table v-if="Array.isArray(value)" striped hover :items="value"></b-table></li>    
-        </ol>
-    </b-container>
+  <b-container>
+    <h1>User data</h1>
+    <ol class="list-group list-group-numbered">
+      <li v-for="(value, key) in user" class="list-group-item"><strong>{{key | upperCase}}:</strong> {{value | isEmpty |
+      isArray}} <b-table v-if="Array.isArray(value)" striped hover :items="value"></b-table>
+      </li>
+    </ol>
+  </b-container>
 </template>
-
-
-<script>
-    export default{
-        filters: {
-    isEmpty: function (value) {
-    if (!value || value.length==0) return '-No data-'
-    return value
-  },
-
-  isArray: function (value) {
-    if (Array.isArray(value)) return ''
-    return value
-  },
-
-  upperCase: function (value) {
-    return value.toUpperCase();
-  },
-
-}
-    }
-</script>
